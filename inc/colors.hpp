9b8b684fd566e772dd9d94d3bdf7136b42ebaa72
raweber42 <ralf297@protonmail.com>
2022-11-10 08:46:22 +0100

    constructor tests working, only == etc left over

---
+++ inc/colors.hpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   colors.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: raweber <raweber@student.42wolfsburg.de    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/10 07:55:35 by raweber           #+#    #+#             */
+/*   Updated: 2022/11/10 08:29:28 by raweber          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef COLORS_HPP
+# define COLORS_HPP
+# define COLOR_DEFAULT "\033[0m"
+# define COLOR_RED "\033[31;1m"
+# define COLOR_GREEN "\033[32;1m"
+# define COLOR_YELLOW "\033[33;1m"
+# define COLOR_BLUE "\033[34;1m"
+# define COLOR_MAGENTA "\033[35;1m"
+# define COLOR_CYAN "\033[36;1m"
+# define COLOR_WHITE "\033[37;1m"
+# ifndef DEBUG
+#  define DEBUG 0
+# endif
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <deque>
 #include <unistd.h>
+#include "../inc/colors.hpp"
 #if 1 //CREATE A REAL STL EXAMPLE
 	#include <map>
 	#include <stack>
@@ -23,44 +24,64 @@ struct Buffer
 	char buff[BUFFER_SIZE];
 };
 
-void print_values(ft::vector<int> vector_int) {
+void print_values(ft::vector<int> v1) {
 	
-	std::cout << "The values held by the vector are  (accessed via iterator): " << std::endl;
-	for (ft::vector<int>::iterator it = vector_int.begin(); it != vector_int.end(); it++)
+	std::cout << "The values held by the vector are (accessed via iterator): [ ";
+	for (ft::vector<int>::iterator it = v1.begin(); it != v1.end(); it++)
 	{
-		std::cout << *it << ", ";
+		std::cout << *it << " ";
 		fflush(stdout);
 		usleep(80000);
 	}
-	std::cout << "FINISHED" << std::endl << std::endl;
+	std::cout << "]" << std::endl << std::endl;
 }
 
-void print_values_reversed(ft::vector<int> vector_int) {
+void print_values_reversed(ft::vector<int> v1) {
 	
-	std::cout << "The values held by the vector are (accessed via REVERSE iterator): " << std::endl;
-	for (ft::vector<int>::reverse_iterator it = vector_int.rbegin(); it != vector_int.rend(); it++)
+	std::cout << "The values held by the vector are (accessed via REVERSE iterator): [ ";
+	for (ft::vector<int>::reverse_iterator it = v1.rbegin(); it != v1.rend(); it++)
 	{
-		std::cout << *it << ", ";
+		std::cout << *it << " ";
 		fflush(stdout);
 		usleep(80000);
 	}
-	std::cout << "FINISHED" << std::endl << std::endl;
+	std::cout << "]" << std::endl << std::endl;
 }
 
 
 int main(void) {
 
-	std::cout << std::endl << "\033[33m" << "######################## INTEGER VECTOR ###############################" << "\033[0m" << std::endl;
-	
-	usleep(500000);
-	std::cout << "\033[34m" <<  "!! ADD TESTS FOR CONSTRUCTORS HERE !!" << "\033[0m" << std::endl;
-	ft::vector<int> zeroed_vector(10);
-	usleep(500000);
-
-	ft::vector<int> vector_int;
 	int test_size = 20;
+	std::cout << std::endl << COLOR_YELLOW << "######################## INTEGER VECTOR ###############################" << COLOR_DEFAULT << std::endl;
 	
-	std::cout << "\033[34m" << "Pushing integers 0 - " << (test_size - 1) << " to vector" << "\033[0m";
+	usleep(500000);
+	std::cout << COLOR_RED <<  "CONSTRUCTOR TESTS" << COLOR_DEFAULT << std::endl;
+	std::cout << COLOR_BLUE << "Initializing empty vector v1" << COLOR_DEFAULT << std::endl;
+	ft::vector<int> v1;
+	print_values(v1);
+
+	std::cout << COLOR_BLUE << "Initializing vector v2 with 5 values of '5' via 'v2(5, 5)'" << COLOR_DEFAULT  << std::endl;
+	ft::vector<int> v2(size_t(5), int(5));
+	print_values(v2);
+
+	std::cout << COLOR_BLUE << "Initializing vector v3 with iterator range via 'v3(v2.begin() + 1, v2.end() - 1)'" << COLOR_DEFAULT  << std::endl;
+	ft::vector<int>::iterator tmp_it_begin = v2.begin() + 1;
+	ft::vector<int>::iterator tmp_it_end = v2.end() - 1;
+	ft::vector<int> v3(tmp_it_begin, tmp_it_end);
+	print_values(v3);
+
+	std::cout << COLOR_BLUE << "Initializing vector v4 via copy constructor via 'v4(v3)'" << COLOR_DEFAULT  << std::endl;
+	ft::vector<int> v4(v3);
+	print_values(v4);
+
+	std::cout << COLOR_BLUE << "Initializing vector v5 via assignment 'v5 = v4'" << COLOR_DEFAULT  << std::endl;
+	ft::vector<int> v5 = v4;
+	print_values(v5);
+	usleep(500000);
+	
+	usleep(500000);
+	std::cout << COLOR_RED <<  "FILL AND SHRINK V1" << COLOR_DEFAULT << std::endl;
+	std::cout << COLOR_BLUE << "Pushing integers 0 - " << (test_size - 1) << " to v1" << COLOR_DEFAULT;
 	for (int i = 0; i < 5; i++)
 	{
 		std::cout << " .";
@@ -70,103 +91,97 @@ int main(void) {
 	std::cout << std::endl;
 	for (int i = 0; i < test_size; i++)
 	{
-		vector_int.push_back(i);
+		v1.push_back(i);
 	}
-	print_values(vector_int);
-	print_values_reversed(vector_int);
+	print_values(v1);
+	print_values_reversed(v1);
 
-	std::cout << "\033[34m" <<  "Popping half of the values" << "\033[0m";
+	std::cout << COLOR_BLUE <<  "Popping half of the values from v1" << COLOR_DEFAULT;
 	for (int i = 0; i < (test_size/2); i++)
 	{
 		std::cout << " .";
 		fflush(stdout);
-		vector_int.pop_back();
+		v1.pop_back();
 		usleep(200000);
 	}
 	std::cout << std::endl;
-	print_values(vector_int);
-
-	usleep(500000);
-	std::cout << "\033[34m" << "Copying values to new vector via assignment and copy assignment " << "\033[0m" << std::endl;
-	ft::vector<int> copy_assign_vector = vector_int;
-	ft::vector<int> assign_vector;
-	assign_vector = vector_int;
-	std::cout << "Print copy assignment vector:" << std::endl;
-	print_values(copy_assign_vector);
-	std::cout << "Print assignment vector:" << std::endl;
-	print_values(assign_vector);
+	print_values(v1);
 	usleep(500000);
 
-	std::cout << "\033[34m" << "Check '==' operator overload" << "\033[0m" << std::endl;
-	std::cout << "Is vector_int == copy_assign_vector? " << ((vector_int == copy_assign_vector) ? "YES" : "NO") << std::endl;
-	std::cout << "Is vector_int == assign_vector? " << ((vector_int == copy_assign_vector) ? "YES" : "NO") << std::endl;
 
 	usleep(500000);
-	std::cout << "\033[34m" <<  "TESTS FOR ELEMENT ACCESS" << "\033[0m" << std::endl;
-	std::cout << "\033[34m" << "Get first value via 'front'" << "\033[0m" << std::endl;
-	std::cout << "First value is: " << vector_int.front() << std::endl;
-	std::cout << "\033[34m" << "Get last value via 'back'" << "\033[0m" << std::endl;
-	std::cout << "Last value is: " << vector_int.back() << std::endl;
-	std::cout <<  "\033[34m" << "Get first value via vector_int.at(0)" << "\033[0m" << std::endl;
-	std::cout << "Value is: " << vector_int.at(0) << std::endl;
-	std::cout <<  "\033[34m" << "Get first value via vector_int[0]" << "\033[0m" << std::endl;
-	std::cout << "Value is: " << vector_int[0] << std::endl;
-	std::cout <<  "\033[34m" << "Get all pointer to first container element via vector_int.data()" << "\033[0m" << std::endl;
-	std::cout << "Address is: " << vector_int.data() << " which holds the value of " << *vector_int.data() << std::endl << std::endl;
+	std::cout << COLOR_RED <<  "TESTS FOR ELEMENT ACCESS" << COLOR_DEFAULT << std::endl;
+	std::cout << COLOR_BLUE << "Get first value via ' v1.front()'" << COLOR_DEFAULT << std::endl;
+	std::cout << "First value is: " << v1.front() << std::endl;
+	std::cout << COLOR_BLUE << "Get last value via 'v1.back()'" << COLOR_DEFAULT << std::endl;
+	std::cout << "Last value is: " << v1.back() << std::endl;
+	std::cout <<  COLOR_BLUE << "Get first value via 'v1.at(0)'" << COLOR_DEFAULT << std::endl;
+	std::cout << "Value is: " << v1.at(0) << std::endl;
+	std::cout <<  COLOR_BLUE << "Get first value via 'v1[0]'" << COLOR_DEFAULT << std::endl;
+	std::cout << "Value is: " << v1[0] << std::endl;
+	std::cout <<  COLOR_BLUE << "Get pointer to first container element via 'v1.data()'" << COLOR_DEFAULT << std::endl;
+	std::cout << "Address is: " << v1.data() << " which holds the value of " << *v1.data() << std::endl << std::endl;
 	usleep(500000);
 
 	usleep(500000);
-	std::cout << "\033[34m" <<  "TESTS FOR CAPACITY FUNCTIONS" << "\033[0m" << std::endl;
-	std::cout << "\033[34m" << "Check if container is empty via vector_int.empty()" << "\033[0m" << std::endl;
-	std::cout << "Function returns: " << ((vector_int.empty()) ? "TRUE" : "FALSE") << std::endl;
-	std::cout << "\033[34m" << "Get size of vector via 'size()'" << "\033[0m" << std::endl;
-	std::cout << "Current size is is: " << vector_int.size() << std::endl;
-	std::cout <<  "\033[34m" << "Get max size of vector via 'max_size()'" << "\033[0m" << std::endl;
-	std::cout << "Max size is: " << vector_int.max_size() << std::endl;
-	std::cout <<  "\033[34m" << "Get capacity of vector via capacity()" << "\033[0m" << std::endl;
-	std::cout << "Current capacity is: " << vector_int.capacity() << std::endl;
-	std::cout <<  "\033[34m" << "Try calling 'vector_int.reserve(5)' (should NOT increase capacity)" << "\033[0m" << std::endl;
-	vector_int.reserve(5);
-	std::cout << "Current capacity is: " << vector_int.capacity() << std::endl;
-	std::cout <<  "\033[34m" << "Try calling 'vector_int.reserve(50)' (SHOULD increase capacity)" << "\033[0m" << std::endl;
-	vector_int.reserve(50);
-	std::cout << "Current capacity is: " << vector_int.capacity() << std::endl << std::endl;
+	std::cout << COLOR_RED <<  "TESTS FOR CAPACITY FUNCTIONS" << COLOR_DEFAULT << std::endl;
+	std::cout << COLOR_BLUE << "Check if container is empty via 'v1.empty()'" << COLOR_DEFAULT << std::endl;
+	std::cout << "Function returns: " << ((v1.empty()) ? "TRUE" : "FALSE") << std::endl;
+	std::cout << COLOR_BLUE << "Get size of vector via 'v1.size()'" << COLOR_DEFAULT << std::endl;
+	std::cout << "Current size is is: " << v1.size() << std::endl;
+	std::cout <<  COLOR_BLUE << "Get max size of vector via 'max_size()'" << COLOR_DEFAULT << std::endl;
+	std::cout << "Max size is: " << v1.max_size() << std::endl;
+	std::cout <<  COLOR_BLUE << "Get capacity of vector via 'v1.capacity()'" << COLOR_DEFAULT << std::endl;
+	std::cout << "Current capacity is: " << v1.capacity() << std::endl;
+	std::cout <<  COLOR_BLUE << "Try calling 'v1.reserve(5)' (should NOT increase capacity)" << COLOR_DEFAULT << std::endl;
+	v1.reserve(5);
+	std::cout << "Current capacity is: " << v1.capacity() << std::endl;
+	std::cout <<  COLOR_BLUE << "Try calling 'v1.reserve(50)' (SHOULD increase capacity)" << COLOR_DEFAULT << std::endl;
+	v1.reserve(50);
+	std::cout << "Current capacity is: " << v1.capacity() << std::endl << std::endl;
 	usleep(500000);
 
 	usleep(500000);
-	std::cout << "\033[34m" <<  "TESTS FOR MODIFIER FUNCTIONS" << "\033[0m" << std::endl;
-	std::cout << "\033[34m" << "Resize the vector via vector_int.resize()" << "\033[0m" << std::endl;
-	std::cout << "Current capacity is: " << vector_int.capacity() << std::endl;
-	std::cout << "\033[34m" << "Insert 5 elements of value '66' at the beginning of the vector via 'vector_int.insert(vector_int.begin(), 5, 66)'" << "\033[0m" << std::endl;
-	vector_int.insert(vector_int.begin(), 5, 66);
-	print_values(vector_int);
+	std::cout << COLOR_RED <<  "TESTS FOR MODIFIER FUNCTIONS" << COLOR_DEFAULT << std::endl;
+	std::cout << COLOR_BLUE << "Resize the vector via v1.resize(10)" << COLOR_DEFAULT << std::endl;
+	v1.resize(10);
+	print_values(v1);
+	std::cout << COLOR_BLUE << "Resize the vector via v1.resize(20, 5)" << COLOR_DEFAULT << std::endl;
+	v1.resize(20, 5);
+	print_values(v1);
+	std::cout << COLOR_BLUE << "Insert 5 elements of value '66' at the beginning of the vector via 'v1.insert(v1.begin(), 5, 66)'" << COLOR_DEFAULT << std::endl;
+	v1.insert(v1.begin(), 5, 66);
+	print_values(v1);
 	// maybe more insert tests here!
-	std::cout << "\033[34m" << "Erase the first five elements at the beginning of the vector via calling 'vector_int.erase(vector_int.begin())' five times" << "\033[0m" << std::endl;
+	std::cout << COLOR_BLUE << "Erase the first five elements at the beginning of the vector via calling 'v1.erase(v1.begin())' five times" << COLOR_DEFAULT << std::endl;
 	for (int i = 0; i < 5; i++)
-		vector_int.erase(vector_int.begin());
-	print_values(vector_int);
-	std::cout << "\033[34m" << "Swap zeroed vector and vector_int via vector_int.swap(zeroed_vector)" << "\033[0m" << std::endl;
-	vector_int.swap(zeroed_vector);
-	std::cout << "Print values of vector_int:" << std::endl;
-	print_values(vector_int);
-	std::cout << "Print values of zeroed_vector:" << std::endl;
-	print_values(zeroed_vector);
-	std::cout << "\033[34m" << "Clear the vector via vector.clear()" << "\033[0m" << std::endl;
-	vector_int.clear();
-	std::cout << "Current size is is: " << vector_int.size() << std::endl;
-	std::cout << "Current capacity is: " << vector_int.capacity() << std::endl;
+		v1.erase(v1.begin());
+	print_values(v1);
+	std::cout << COLOR_BLUE << "Swap v1 and v2 via v1.swap(v2)" << COLOR_DEFAULT << std::endl;
+	v1.swap(v2);
+	std::cout << "Print values of v1:" << std::endl;
+	print_values(v1);
+	std::cout << "Print values of v2:" << std::endl;
+	print_values(v2);
+	std::cout << COLOR_BLUE << "Clear the vector via vector.clear()" << COLOR_DEFAULT << std::endl;
+	v1.clear();
+	std::cout << "Current size is is: " << v1.size() << std::endl;
+	std::cout << "Current capacity is: " << v1.capacity() << std::endl;
 	usleep(500000);
 
 
 
+	// std::cout << COLOR_BLUE << "Check '==' operator overload" << COLOR_DEFAULT << std::endl;
+	// std::cout << "Is v1 == copy_assign_vector? " << ((v1 == copy_assign_vector) ? "YES" : "NO") << std::endl;
+	// std::cout << "Is v1 == assign_vector? " << ((v1 == copy_assign_vector) ? "YES" : "NO") << std::endl;
 
 
 
-	// std::cout << "\033[33m" << "//######################## STRING VECTOR ######################################" << "\033[0m" << std::endl;
+	// std::cout << COLOR_YELLOW << "//######################## STRING VECTOR ######################################" << COLOR_DEFAULT << std::endl;
 	// std::cout << std::endl << "Checking string vector" << std::endl;
 	// ft::vector<std::string> vector_str;
 
-	// std::cout << "\033[33m" << "//######################## CUSTOM STRUCT VECTOR ###############################" << "\033[0m" << std::endl;
+	// std::cout << COLOR_YELLOW << "//######################## CUSTOM STRUCT VECTOR ###############################" << COLOR_DEFAULT << std::endl;
 
 	// std::cout << std::endl << "Checking custom struct vector" << std::endl;
 	// ft::vector<Buffer> vector_buffer;
